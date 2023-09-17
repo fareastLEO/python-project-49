08f46c5971b271301bdb16f96daf4dac5171eaad
fareast.leo <fareast.leo@yandex.ru>
2023-09-17 21:52:43 +1000

    changed files brain_games.py and pyproject.toml

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,3 +1,5 @@
 def main():
-    print("poetry run python -m brain_games.scripts.brain_games")
-    print("Welcome to the Brain Games")
+    print("Welcome to the Brain Games!")
+
+if __name__ == '__main__':
+    main()
