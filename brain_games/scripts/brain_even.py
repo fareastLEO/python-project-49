92700eac747c8039ffe36dd7cc1852d0fea87933
fareast.leo <fareast.leo@yandex.ru>
2023-10-03 03:02:26 +1000

    edit: nothing

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from brain_games.game_module.welcome import welcome
-from brain_games.game_module.game_even import brain_even
+from brain_games.games.welcome import welcome
+from brain_games.games.game_even import brain_even
 
 
 def main():
