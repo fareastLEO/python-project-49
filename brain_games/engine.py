a58bf4c87229bcffb4d69fb20a25d8a74bcb48b2
fareast.leo <fareast.leo@yandex.ru>
2024-05-22 10:48:56 +1000

    Created a constant to indicate the number of game attempts

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,20 +1,20 @@
 import prompt
 
+GAME_ATTEMPTS = 3  # Set the number of game attempts
+
 
 def game_engine(game_module):
     print("Welcome to the Brain Games!")
     username = prompt.string('May I have your name? ')
     print(f'Hello, {username}')
     print(game_module.GAME_TASK)
-    i = 0
-    while i < 3:
+    for i in range(GAME_ATTEMPTS):
         game_question, game_answer = game_module.game_logic()
         print(f'Question: {game_question}')
         user_answer = prompt.string('Your answer: ')
         user_answer = user_answer.lower().strip()
         if user_answer == game_answer:
             print('Correct!')
-            i += 1
             continue
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
